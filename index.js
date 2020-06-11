7cb2e461b1ab8e81916d1374fb54b0741243e06f
vj_akash <48998005+vjakash@users.noreply.github.com>
2020-06-11 19:51:24 +0530

    task completed

---
+++ index.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+const express = require('express');
+const path = require('path')
+const app = express();
+
+let server = app.listen(3000, function() {
+    // let host = server.address().address;
+    // let port = server.address().port;
+    // console.log("Text File app listening at http://%s:%s", host, port);
+    console.log("Text File app listening at 3000");
+});
+
+app.get('/create', (req, res) => {
+    let today = new Date();
+    let date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
+    let time = today.getHours() + "-" + today.getMinutes() + "-" + today.getSeconds();
+    let filename = `${date}(${time})`;
+    fs.writeFile(`${filename}.txt`, `Created at - ${date} ${time}`, 'utf8', (err) => {
+        if (err) throw err;
+        res.send("filecreated");
+    })
+});
+app.get('/textfile', (req, res) => {
+    let txtFiles = [];
+    fs.readdir('D:/Vijay Akash/Node js/Intro to node and express', (err, data) => {
+        if (err) throw err;
+        data.forEach((item) => {
+            if (String(path.extname(item)) == '.txt') {
+                txtFiles.push(item);
+            }
+        })
+        res.json(txtFiles);
+    })
+});
